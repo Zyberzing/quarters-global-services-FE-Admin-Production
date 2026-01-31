52de9bb3573d0c8dc58ed91af34715cd1f24717d
manishsaurabhinfosys <manish@saurabhinfosys.com>
2026-02-01 00:07:28 +0530

    chore: Remove actions buttons

--- app/admin/(dashboard)/applications/edit/Actions.tsx
+++ app/admin/(dashboard)/applications/edit/Actions.tsx
@@ -1,14 +1,12 @@
-import { Button } from '@/components/ui/button';
-import { Printer } from 'lucide-react';
 import React from 'react';
 
 const Actions = () => {
   return (
     <div className="flex items-center gap-2">
-      <Button>Generate Cover Letter</Button>
+      {/* <Button>Generate Cover Letter</Button>
       <Button>
         <Printer /> Print
-      </Button>
+      </Button> */}
     </div>
   );
 };
